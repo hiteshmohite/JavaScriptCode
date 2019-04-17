a8893589f2feabc0592422c59fba2decea9ed1b3
hiteshmohite <hm7181@g.rit.edu>
2019-04-17 00:12:57 -0500

    first commit on script.js after No:9

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,19 @@
 console.log("Hello World!!!");
+
+var first = "Hitesh";
+console.log(first);
+
+var last = "Mohite";
+console.log(last);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Software Developer";
+console.log(job);
